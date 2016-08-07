1e1d085e1f750fd2fed378dffcda5dc0a66ed47b
powerpoint45 <231447@gmail.com>
2016-08-06 19:45:26 -0500

    browser intent fixes. Apk update. UI changes

--- app/build/generated/source/buildConfig/debug/com/powerpoint45/lucidbrowser/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/powerpoint45/lucidbrowser/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.powerpoint45.lucidbrowser";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 46;
-  public static final String VERSION_NAME = "4.89";
+  public static final int VERSION_CODE = 47;
+  public static final String VERSION_NAME = "4.891";
 }
--- app/src/main/java/com/powerpoint45/lucidbrowser/SystemBarTintManager.java
+++ app/src/main/java/com/powerpoint45/lucidbrowser/SystemBarTintManager.java
@@ -63,6 +63,7 @@ public class SystemBarTintManager {
      * @param activity The host activity.
      */
     @TargetApi(19)
+    @SuppressWarnings("ResourceType")
     public SystemBarTintManager(Activity activity) {
 
         Window win = activity.getWindow();
